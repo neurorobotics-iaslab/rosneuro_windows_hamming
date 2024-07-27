e5ad423c13731a7878369e48fe4cc97fd2a56415
mirovix <miroljubmihailovic98@gmail.com>
2024-07-27 16:19:57 +0200

    feat: creating dependency on window

--- src/Hamming.cpp
+++ src/Hamming.cpp
@@ -1,7 +1,7 @@
 #include "rosneuro_windows_hamming/Hamming.hpp"
 #include "pluginlib/class_list_macros.h"
 
-PLUGINLIB_EXPORT_CLASS(rosneuro::Hamming<int>,    rosneuro::Filter<int>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::Hamming<float>,  rosneuro::Filter<float>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::Hamming<double>, rosneuro::Filter<double>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Hamming<int>,    rosneuro::Window<int>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Hamming<float>,  rosneuro::Window<float>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Hamming<double>, rosneuro::Window<double>)
 
